491def1a49b114d52250c4d8d85d57af6985ce09
eloyse.fernanda <eloyse.fernanda@g.globo>
2023-11-28 12:33:55 -0300

    criando locations e metricas para cada

--- ultima-tarefa/app.py
+++ ultima-tarefa/app.py
@@ -1,10 +1,13 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 from prometheus_client import Counter, Gauge, generate_latest
 import random
 
 app = Flask(__name__)
 
 CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
+
+def random_number():
+    return str(random.choice([200, 300, 500]))
 
 numero_requests = Counter(
     'app_flask_numero_requests',
@@ -15,21 +18,39 @@ numero_requests = Counter(
 memoria_usada = Gauge(
     'memoria_usada',
     'A memoria que esta sendo usada (aleatoriamente).',
-    ['server_name']
+    ['server_name', 'location']
 )
-@app.route('/')
+
+# Metrics specific to locations
+location_requests = Counter(
+    'location_requests',
+    'Number of requests for each location.',
+    ['status_code']
+)
+
+# location_response_time = Gauge(
+#     'location_response_time',
+#     'Response time for each location.',
+#     ['location']
+# )
+
+@app.route('/start')
 def start():
-    return "hello"
+    valor = random_number()
+    numero_requests.labels(valor).inc()
+    memoria_usada.labels('server-a', 'principal').set(random.randint(10000, 90000))
+
+    return "princiapl"
+
+@app.route('/anyeon')
+def anyeon():
+    location_requests.labels(random_number()).inc()
+    memoria_usada.labels('server-a', 'anyeon')
+    return 'anyeon'
 
 
 @app.route('/metrics', methods=['GET'])
 def get_data():
-    #random status code
-    random_status_code = str(random.choice([200,300,500]))
-
-    numero_requests.labels(random_status_code).inc()
-    memoria_usada.labels('server-a').set(random.randint(10000,90000))
-    
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
